1c6e040082d7a0487a35a5ef6e5236a5ea059046
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-07-08 13:14:51 +0200

    chore: temporary journal animations

--- src/app/dashboard/journal/page.tsx
+++ src/app/dashboard/journal/page.tsx
@@ -1,4 +1,6 @@
-import { useMemo } from 'react';
+'use client';
+
+import { useMemo, useRef, useState } from 'react';
 import DashboardHeader from '~/components/DashboardHeader';
 
 const testData = [
@@ -69,8 +71,12 @@ function groupByMonth(data: JournalEntry[]) {
     return grouped;
 }
 
-function JournalBaseItem({ className, children }: { className?: string; children?: React.ReactNode }) {
-    return <div className={['flex h-4 items-center', className].join(' ')}>{children}</div>;
+function JournalBaseItem({ className, children, id }: { className?: string; children?: React.ReactNode; id?: string }) {
+    return (
+        <div className={['flex h-4 items-center', className].join(' ')} id={id}>
+            {children}
+        </div>
+    );
 }
 
 function DateItem({ date }: { date: Date }) {
@@ -87,22 +93,66 @@ function DateItem({ date }: { date: Date }) {
     );
 }
 
+const normalMultiplier = 0.5;
+const adjacentMultiplier = 0.75;
+const heldMultiplier = 1;
 function JournalGroup({ month, entries }: { month: string; entries: JournalEntry[] }) {
+    const [hovered, setHovered] = useState<number | null>(null);
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const groupedWidths = useMemo(() => {
+        const maxWidth = containerRef.current?.clientWidth ?? 0;
+        const obj = { [month]: entries };
+        const widths = Object.entries(obj).reduce(
+            (acc, [month, entries]) => {
+                const total = entries.reduce((acc, entry) => acc + entry.count, 0);
+                acc[month] = {
+                    total,
+                    max: Math.max(...entries.map((entry) => entry.count)),
+                    min: Math.min(...entries.map((entry) => entry.count)),
+                    widths: [],
+                };
+                const widths = entries.map((entry, idx) => {
+                    const width = (entry.count / acc[month]!.max) * maxWidth;
+                    const isAdjacent = hovered === idx + 1 || hovered === idx - 1;
+
+                    const multiplier = isAdjacent
+                        ? adjacentMultiplier
+                        : idx === hovered
+                          ? heldMultiplier
+                          : normalMultiplier;
+                    return width * multiplier;
+                });
+                return { [month]: { ...acc[month], widths } };
+            },
+            {} as Record<string, { total: number; max: number; min: number; widths: number[] }>,
+        );
+
+        return widths;
+    }, [month, entries, hovered]);
+
     return (
         <div className='flex w-full grow-0'>
-            <div className='w-44 border-t border-neutral-500 pt-2'>
+            <div className='w-44 flex-none grow-0 border-t border-neutral-500 pt-2'>
                 <p className='font-grotesk text-3xl'>{month}</p>
             </div>
             {/* We should be using a grid, but flex works so whatever */}
-            <div className='flex w-12 grow-0 flex-col gap-2 border-t border-neutral-500 pt-2'>
+            <div className='flex w-12 flex-none grow-0 flex-col gap-2 border-t border-neutral-500 pt-2'>
                 {entries.map((entry) => (
                     <DateItem date={entry.date} key={entry.date.toString()} />
                 ))}
             </div>
-            <div className='flex grow flex-col gap-2 pt-2'>
-                {entries.map((entry) => (
+            <div ref={containerRef} className='flex grow flex-col gap-2 pt-2'>
+                {entries.map((entry, idx) => (
                     <JournalBaseItem className='px-4' key={entry.date.toString()}>
-                        <div className='h-3 w-full rounded-sm bg-accent'></div>
+                        <div
+                            style={{
+                                width: `${groupedWidths[month]?.widths[idx] ?? 0}px`,
+                            }}
+                            onMouseEnter={() => setHovered(idx)}
+                            onMouseLeave={() => setHovered(null)}
+                            className='h-3 w-full rounded-sm bg-accent transition-all'
+                        ></div>
                     </JournalBaseItem>
                 ))}
             </div>
@@ -110,7 +160,7 @@ function JournalGroup({ month, entries }: { month: string; entries: JournalEntry
     );
 }
 
-export default async function Journal() {
+export default function Journal() {
     const grouped = useMemo(() => groupByMonth(testData), []);
 
     return (
